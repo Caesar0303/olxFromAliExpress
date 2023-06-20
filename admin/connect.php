596a4088086d91e22a85fd4a4622e376cb56f3c2
Caesar0303 <galaaramaa@gmail.com>
2023-06-20 19:58:38 +0600

    update

--- admin/connect.php
+++ admin/connect.php
@@ -1,6 +1,15 @@
 <?php 
     session_start();
-    $connect = mysqli_connect('localhost','root','','olx');
+    $envPath = __DIR__ . '/../.env';
+    $config = file_get_contents($envPath);
+    $config = explode("\n",$config);
+    $env=[];
+    foreach ($config as $row) {
+        $item = explode("=",$row);
+        $env[$item[0]] = trim($item[1]);
+    }
+
+    $connect = mysqli_connect('localhost',$env['DB_LOGIN'],$env['DB_PASSWORD'],$env['DB_NAME']);
     if(!$connect) {
         die('Ошибка');
     }
@@ -8,12 +17,7 @@
     if (isset($_SESSION['user'])) {
         $login = $_SESSION['user'];
         $id = mysqli_query($connect, "SELECT id FROM users WHERE login = '$login'");
+        $id = mysqli_fetch_all($id);
+        $id = $id[0][0];
     }
-    
-    if ($_SESSION['user'] == NULL) {
-        header ('Location: authAndReg/auth.php');
-    }
-
-    $id = mysqli_fetch_all($id);
-    $id = $id[0][0];
 ?>
